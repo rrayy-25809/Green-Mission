e77fdcbf3bdad7c0e63ad1b785d784118c3e791f
rrayy <rrayy25809@gmail.com>
2025-06-02 15:40:55 +0900

    Changes to be committed: 디자인 일부 수정, 추가, 기념일 정보 가져올 파일 생성 	modified:   client/index.html 	modified:   client/src/style.css 	new file:   server/routes/today.py 	modified:   server/test.py

---
+++ server/routes/today.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, redirect, render_template, session, current_app
+from server.db import NotionDatabase
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+# 플라스크 기본 설정
+bp = Blueprint('today', __name__)
+
+# 데이터베이스 설정
+load_dotenv()
+daylist_db = NotionDatabase(os.getenv("DAYLIST_DB_ID"))
+
+@bp.route('/today', methods=['POST'])
+def today():
+    today = datetime.now().strftime("%Y-%m-%d") + '+0900'
+    for i in daylist_db.get_page_ids():
+        page = daylist_db.get_page_properties(i)
+        # page.result["날짜"]["date"]["start"] 값이 "2025-05-28" 같은 문자열이라고 가정
+        anniversary_date = page.result["날짜"]["date"]["start"]["start"]
+        # If anniversary_date_value is a dict, extract the 'start' key; otherwise, use as is
+        anniversary_date = datetime.strptime(anniversary_date, "%Y-%m-%d").date()
+        today_date = datetime.now().date()
+
+        if anniversary_date == today_date:
+            current_app.logger.info("오늘의 기념일이 있습니다.")
+            return page.result["기념일 명"]["title"][0]["text"]["content"]
--- server/test.py
+++ server/test.py
@@ -1,7 +1,7 @@
 from db import NotionDatabase
 from datetime import datetime
 
-user_db = NotionDatabase("1f8dc33ef503801e81b0df64081aba0e")
+user_db = NotionDatabase("206dc33ef503803da0d9e88586149f90")
 
 # print(user_db.get_database_properties())
 
@@ -15,9 +15,19 @@ user_db = NotionDatabase("1f8dc33ef503801e81b0df64081aba0e")
 # }
 # result = user_db.create_database_page(properties)
 # print(result)
-# import json
-# user_list = user_db.get_pages_id()
+import json
+user_list = user_db.get_page_ids()
 # with open("test.json", "w", encoding="utf-8") as f:
-#     json.dump(user_db.get_page_properties(user_list[1]).result, f, ensure_ascii=False, indent=4)
+#     json.dump(user_db.get_page_properties(user_list[0]).result, f, ensure_ascii=False, indent=4)
+# page = user_db.get_page_properties(user_list[0])
+# today = datetime.now().strftime("%Y-%m-%d") + '+0900'
+# # page.result["날짜"]["date"]["start"] 값이 "2025-05-28" 같은 문자열이라고 가정
+# anniversary_date = page.result["날짜"]["date"]["start"]["start"]
+# # If anniversary_date_value is a dict, extract the 'start' key; otherwise, use as is
+# anniversary_date = datetime.strptime(anniversary_date, "%Y-%m-%d").date()
+# today_date = datetime.now().date()
+
+# if anniversary_date == today_date:
+#     print("오늘의 기념일이 있습니다.")
 
 # print(user_db.get_page_properties(user_list[1]))
