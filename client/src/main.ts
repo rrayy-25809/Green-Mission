98912ce12d8ba5ad1836b8e4ae650a96b7da7975
rrayy <rrayy25809@gmail.com>
2025-06-05 09:50:30 +0900

    Changes to be committed: scss로 css 마이그레이션 	modified:   client/calendar.html 	modified:   client/index.html 	modified:   client/login.html 	modified:   client/mypage.html 	modified:   client/src/main.ts 	renamed:    client/src/style.css -> client/src/style.scss 	modified:   package-lock.json 	modified:   package.json

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 // 페이지 제목 설정
 const path = window.location.pathname;
