b35f0b711a01548d1a178a224606a17e3ead406e
rrayy <rrayy25809@gmail.com>
2025-06-09 13:39:11 +0900

    Changes to be committed: 캘린더 아이디어 제거, 메인 페이지 챌린지 동적로딩 코드 추가 	deleted:    client/calendar.html 	new file:   client/make_challenge.html 	modified:   client/src/challenge.scss 	modified:   client/src/challenge.ts 	modified:   server/routes/challenge.py

--- client/src/challenge.ts
+++ client/src/challenge.ts
@@ -20,40 +20,43 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     if (response.ok) {
         challenge_list.innerHTML = ''; // 로딩 표시를 제거하고 챌린지 목록을 비웁니다.
-        //const responseText = await response.text();
-        const challenge = `<div class="row g-0 challenge-item">
-            <div class="col-auto challenge-img-box">
-                <img src="src/images/challenge.png" alt="챌린지 이미지 1">
-            </div>
-            <div class="col challenge-main">
-                <div class="row challenge-header">
-                    <div class="col-auto challenge-title">
-                        <h3>${'챌린지_이름'}</h3>
+        const data = await response.json();
+        for (let index = 0; index < data.length; index++) {
+            const i = data[index];
+            const challenge = `<div class="row g-0 challenge-item">
+                <div class="col-auto challenge-img-box">
+                    <img src="${i["챌린지 아이콘"]}" alt="챌린지 이미지 1">
+                </div>
+                <div class="col challenge-main">
+                    <div class="row challenge-header">
+                        <div class="col-auto challenge-title">
+                            <h3>${i["챌린지 제목"]}</h3>
+                        </div>
+                        <div class="col-auto challenge-user">
+                            <span>${i["챌린지 작성자"]}</span>
+                        </div>
                     </div>
-                    <div class="col-auto challenge-user">
-                        <span>${'유저_닉네임'}</span>
+                    <div class="challenge-desc">${i["챌린지 설명"]}</div>
+                    <div class="challenge-hashtag">#${'태준함'}</div>
+                </div>
+                <div class="challenge-divider d-none d-md-block"></div>
+                <div class="col-auto challenge-side">
+                    <span class="challenge-period">참여기간 : ${'2025.06.02~2025.06.04'}</span>
+                    <span class="challenge-participants">현재 참여 인원 : ${1}명</span>
+                    <div class="challenge-side-bottom">
+                        <div class="challenge-cheer">
+                            <i class="bi bi-fire"></i>
+                            <span>1</span>
+                        </div>
+                        <i class="challenge-share bi bi-share"></i>
+                        <a type="button" class="btn challenge-join-btn btn-secondary">참여요청</a>
                     </div>
                 </div>
-                <div class="challenge-desc">${'안녕하세요 반갑습니다 함태준입니다.'}</div>
-                <div class="challenge-hashtag">#${'태준함'}</div>
-            </div>
-            <div class="challenge-divider d-none d-md-block"></div>
-            <div class="col-auto challenge-side">
-                <span class="challenge-period">참여기간 : ${'2025.06.02~2025.06.04'}</span>
-                <span class="challenge-participants">현재 참여 인원 : ${1}명</span>
-                <div class="challenge-side-bottom">
-                    <div class="challenge-cheer">
-                        <i class="bi bi-fire"></i>
-                        <span>1</span>
-                    </div>
-                    <i class="challenge-share bi bi-share"></i>
-                    <a type="button" class="btn challenge-join-btn btn-secondary">참여요청</a>
-                </div>
-            </div>
-        </div>`;
-        challenge_list.appendChild(
-            new DOMParser().parseFromString(challenge, 'text/html').body.firstChild as HTMLElement
-        );
+            </div>`;
+            challenge_list.appendChild(
+                new DOMParser().parseFromString(challenge, 'text/html').body.firstChild as HTMLElement
+            );
+        }
     } else {
         const responseText = await response.text();
         console.error("Error fetching today's challenge:", responseText);
--- server/routes/challenge.py
+++ server/routes/challenge.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, current_app, render_template
 from server.db import NotionDatabase
 from dotenv import load_dotenv
 import os
@@ -8,11 +8,45 @@ bp = Blueprint('challenge', __name__)
 
 # 데이터베이스 설정
 load_dotenv()
-daylist_db = NotionDatabase(os.getenv("CHALLENGE_DB_ID"))
+# challenge_db = NotionDatabase(os.getenv("CHALLENGE_DB_ID"))
+challenge_db = NotionDatabase("20bdc33ef503801e9d94d96404fe33d7")
+user_db = NotionDatabase(os.getenv("USER_DB_ID"))
 
 @bp.route('/challenge/<what_kinda>', methods=['POST'])
 def challenge(what_kinda):
     if what_kinda == "s":
-        return "챌린지 기능은 아직 구현되지 않았습니다."
+        data = []
+        for i in challenge_db.get_page_ids():
+            page = challenge_db.get_page_properties(i)
+
+            try:
+                author_id = user_db.get_page_properties(page.result["챌린지 작성자"]["rich_text"][0]["text"]["content"])
+                author = author_id.result["유저명"]["title"][0]["text"]["content"]
+            except:
+                author = "Green Mission"
+                
+            challenge = {
+                "챌린지 ID" : i,
+                "챌린지 제목": page.result["챌린지 제목"]['title'][0]['text']['content'],
+                "챌린지 작성자" : author,
+                "챌린지 아이콘" : page.result["챌린지 아이콘"]["files"][0]["external"]["url"],
+                "챌린지 설명" : page.result["챌린지 설명"]["rich_text"][0]["text"]["content"],
+            }
+            data.append(challenge)
+
+        return jsonify(data)
     else:
-        return "챌린지 기능은 아직 구현되지 않았습니다."
+        return [{ # 테스트용 더미 데이터
+                "챌린지 ID" : "11111234",
+                "챌린지 제목": "page.result[]['title'][0]['text']['content']",
+                "챌린지 작성자" : "author",
+                "챌린지 아이콘" : "awef",
+                "챌린지 설명" : "awfe",
+            }]
+    
+@bp.route('/make_challenge', methods=['GET', 'POST'])
+def make_challenge():
+    if request.method == "POST":
+        current_app.logger.info("챌린지 만드는 중")
+        return "제작중", 500
+    return render_template("make_challenge.html")
