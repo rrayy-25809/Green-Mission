fd22326732e6983652ef98ca4905d43f0ee5e3c0
rrayy <rrayy25809@gmail.com>
2025-07-03 09:41:22 +0900

    Changes to be committed: 나의 발자국 제작 완료 	modified:   client/mypage.html 	modified:   client/src/mypage.ts 	modified:   server/routes/user.py

--- client/src/mypage.ts
+++ client/src/mypage.ts
@@ -1,4 +1,69 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    const footprint = document.getElementById('my-footprint') as HTMLElement;
+    const circles = footprint.querySelectorAll('circle');
+    const line = footprint.querySelectorAll('line');
+    
+    const challenge_elm = document.getElementsByClassName('challenge-count')[0] as HTMLElement;
+    const challenge_count = challenge_elm?.textContent? Number(challenge_elm.textContent.replace("참여한 챌린지 수 : ", "")): 0;
+    const cheer_challenge = Number(challenge_elm.getAttribute('data-cheer-challenge')) || 0;
+    const make_count = Number(challenge_elm.getAttribute('data-make-challenge')) || 0;
+    
+    const goal = ["그린미션 가입하기", `챌린지 달성하기 ${challenge_count}/1`, `챌린지 응원하기 ${cheer_challenge}/1\n챌린지 달성하기 ${challenge_count}/2`, `챌린지 작성하기 ${make_count}/1`, `챌린지 달성하기 ${challenge_count}/5`, `챌린지 달성하기 ${challenge_count}/8`];
+    const goal_bool = [true, challenge_count >= 1, cheer_challenge >= 1 && challenge_count >= 2, make_count >= 1, challenge_count >= 5, challenge_count >= 8];
+
+    circles.forEach((circle, index) => {
+        if (goal_bool[index]) {
+            circle.setAttribute("stroke","#828282")
+            circle.setAttribute("fill","#4DFF00")
+        } else {
+            circle.setAttribute("stroke","#ADADAD")
+            circle.setAttribute("fill","#E1E1E1")
+        }
+    });
+
+    line.forEach((line, index) => {
+        if (goal_bool[index]) {
+            line.setAttribute("stroke","#828282")
+            line.setAttribute("stroke-width","5")
+        } else {
+            line.setAttribute("stroke","#999999")
+            line.setAttribute("stroke-width","2")
+        }
+    });
+
+    changeprofile();
+    circlehover(goal, goal_bool);
+});
+
+function circlehover(goal:string[], goal_bool:boolean[]) {
+    const footprint = document.getElementById('my-footprint') as HTMLElement;
+    const card = document.querySelector('.goal-card') as HTMLElement;
+    const circles = footprint.querySelectorAll('circle');
+
+    circles.forEach((circle, index) => {
+        circle.addEventListener('mouseenter', () => {
+            card.style.display = 'block'; // 클릭 시 카드 표시
+            console.log(circle.getBoundingClientRect());
+            card.style.top = `${circle.getBoundingClientRect().top + window.scrollY - 250}px`;
+            card.style.left = `${circle.getBoundingClientRect().left + window.scrollX -185}px`;
+
+            const goalText = document.getElementsByClassName('goal-content')[0] as HTMLElement;
+            goalText.innerText = goal[index] || "목표가 설정되지 않았습니다.";
+
+            if (goal_bool[index]) {
+                goalText.style.color = "#4DFF00"; // 목표 달성
+            } else {
+                goalText.style.color = "#FF0000"; // 목표 미달성
+            }
+        });
+
+        circle.addEventListener('mouseleave', () => {
+            card.style.display = 'none'; // 마우스가 떠나면 카드 숨김
+        });
+    });
+}
+
+function changeprofile() {
     const fileinput = document.getElementById('selectedFile') as HTMLInputElement
     
     fileinput.addEventListener('input', async () =>{
@@ -19,24 +84,4 @@ document.addEventListener('DOMContentLoaded', async () => {
             }
         }
     });
-
-    const footprint = document.getElementById('my-footprint') as HTMLElement;
-    const card = document.querySelector('.goal-card') as HTMLElement;
-    const circles = footprint.querySelectorAll('circle');
-
-    circles.forEach((circle, index) => {
-        circle.addEventListener('mouseenter', () => {
-            card.style.display = 'block'; // 클릭 시 카드 표시
-            console.log(circle.getBoundingClientRect());
-            card.style.top = `${circle.getBoundingClientRect().top + window.scrollY - 250}px`;
-            card.style.left = `${circle.getBoundingClientRect().left + window.scrollX -185}px`;
-
-            const goalText = document.getElementsByClassName('goal-content')[0] as HTMLElement;
-            goalText.innerText = `목표 ${index + 1}의 내용입니다. 여기에 목표에 대한 자세한 설명을 추가할 수 있습니다.`;
-        });
-
-        circle.addEventListener('mouseleave', () => {
-            card.style.display = 'none'; // 마우스가 떠나면 카드 숨김
-        });
-    });
-});
+}
--- server/routes/user.py
+++ server/routes/user.py
@@ -11,6 +11,7 @@ bp = Blueprint('user', __name__)
 # 데이터베이스 설정
 load_dotenv()
 user_db = NotionDatabase(os.getenv("USER_DB_ID"))
+challenge_db = NotionDatabase(os.getenv("CHALLENGE_DB_ID"))
 
 @bp.route("/mypage", methods=["GET", "POST"])
 def mypage():
@@ -31,9 +32,23 @@ def user_info(page_id:str) -> dict:
         '사용자_역할': user_info.result["사용자 역할"]["select"]["name"],
         '프로필_사진': user_info.result["프로필 사진"]["files"][0]["file"]["url"] if user_info.result["프로필 사진"]["files"] else None,
         '참여한_챌린지': user_info.result["참여한 챌린지"]["rich_text"][0]["text"]["content"].split(",") if user_info.result["참여한 챌린지"]["rich_text"] else [],
+        '응원_수': len(user_info.result["응원한 챌린지"]["rich_text"][0]["text"]["content"].split(",")) if user_info.result["응원한 챌린지"]["rich_text"] else 0,
+        '챌린지_작성수' : challenge_make_count(page_id),
     }
     
     return properties
+
+def challenge_make_count(page_id):
+    for i in challenge_db.get_page_ids():
+        challenge = challenge_db.get_page_properties(i)
+        count = 0
+        try:
+            if challenge.result["챌린지 작성자"]["rich_text"][0]["text"]["content"] == page_id:
+                count += 1
+        except IndexError:
+            continue
+
+    return count
 
 @bp.route("/delete_account")
 def delete_account():
